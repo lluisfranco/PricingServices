dd3c969e31bbc0b9273911bab2944f0992ead07c
Lluis Franco <lfranco@uyssoft.com>
2020-11-20 12:43:43 +0100

    Added SecurityIdentifierTypeEnum support (Bloomberg)

--- PricingServices.Client.Console/Program.cs
+++ PricingServices.Client.Console/Program.cs
@@ -10,20 +10,16 @@ namespace PricingServices.Client.Console
     {
         static async Task Main()
         {
-            System.Console.WriteLine("Hello Bloomberg API!");
             var bloombergService = Builder.GetPricingService().
                 SetCredentials(new ServiceCredentials()
                 {
                     ClientId = "1e6e5c12b273505793bff2f9df21107f",
-                    ClientSecret = "a1cd3e8a0453cee37635f60f16d2a95a3fe1c807b0dcaf07bdc2ee057638c621",
-                    ExpirationDate = 1652364986947
+                    ClientSecret = "a1cd3e8a0453cee37635f60f16d2a95a3fe1c807b0dcaf07bdc2ee057638c621"
                 }).
                 SetSecuritiesList(new List<SecurityInfo> {
-                    new SecurityInfo() { Name = "AAPL US EQUITY" },
-                    new SecurityInfo() { Name = "AS5533318 CORP" },
-                    new SecurityInfo() { Name = "AN4198411 corp" },
-                    new SecurityInfo() { Name = "EI5630724 GOVT" },
-                    new SecurityInfo() { Name = "AD NA EQuity" },
+                    new SecurityInfo() { Name = "AAPL US Equity" },
+                    new SecurityInfo() { Name = "AS5533318 Corp" },
+                    new SecurityInfo() { Name = "EI5630724 Govt" },
                     new SecurityInfo() { Name = "CAC Index" },
                     new SecurityInfo() { Name = "EUR", Type = SecurityInfoTypeEnum.Currency },
                     new SecurityInfo() { Name = "USD" , Type = SecurityInfoTypeEnum.Currency},
@@ -31,11 +27,6 @@ namespace PricingServices.Client.Console
                     new SecurityInfo() { Name = "AMZN US Equity" },
                     new SecurityInfo() { Name = "GOOG US Equity" },
                     new SecurityInfo() { Name = "GOOGL US Equity" },
-                    new SecurityInfo() { Name = "MERNEWA LX Equity" },
-                    new SecurityInfo() { Name = "MORGBZH LX Equity" },
-                    new SecurityInfo() { Name = "TAGCI SW Equity" },
-                    new SecurityInfo() { Name = "ED630505     Corp" },
-                    new SecurityInfo() { Name = "EP0492991 Pfd" }
                 }).
                 SetFieldsList(new List<string>
                 {
